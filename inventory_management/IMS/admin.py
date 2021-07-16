6a96aedc1aacdbc0cf4e6f2cefdf0868579a903b
AurelliaChristie <aurelliachristie77@gmail.com>
2021-07-16 15:06:21 +0700

    Register models in the admin & start creating views

--- inventory_management/IMS/admin.py
+++ inventory_management/IMS/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+# Import models
+from .models import *
+
+# Register models
+admin.site.register(Product)
+admin.site.register(ProductDetails)
+admin.site.register(Supplier)
+admin.site.register(PurchasedStockInvoice)
+admin.site.register(PurchasedStockDetails)
+admin.site.register(Customer)
+admin.site.register(SoldGoodInvoice)
+admin.site.register(SoldGoodDetails)
